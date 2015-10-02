4168c98f73cb45b488f8532d115e84847e7dc855
Karen Lee <karenlee129@utexas.edu>
2015-10-01 21:53:03 -0500

    new data set (red and white) html

---
+++ 01 Data/white.R
@@ -0,0 +1,11 @@
+require("jsonlite")
+require("RCurl")
+require(extrafont)
+require(ggplot2)
+
+## Data Frame 1
+
+white <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from white_wine"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_kwl377', PASS='orcl_kwl377', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+
+head(white)
+summary(white)
